ced419c137149798fb536db3be22de8e1c9e5398
Sergei <144173692+Vdovenkov-Sergei@users.noreply.github.com>
2024-08-12 19:20:15 +0300

    Catch exceptions & some review

--- main.py
+++ main.py
@@ -10,12 +10,11 @@ field = [deepcopy(base_matrix) for _ in range(SIZE * SIZE)]
 
 def print_field():
     print("-" * WIDTH)
-    for i in range(SIZE):
-        idx = i * SIZE
+    for i in range(0, SIZE * SIZE, SIZE):
         for j in range(SIZE):
-            row_m1 = " ".join(field[idx][j])
-            row_m2 = " ".join(field[idx + 1][j])
-            row_m3 = " ".join(field[idx + 2][j])
+            row_m1 = " ".join(field[i][j])
+            row_m2 = " ".join(field[i + 1][j])
+            row_m3 = " ".join(field[i + 2][j])
             total_row = f"{'|'}{'|'.join([row_m1, row_m2, row_m3])}{'|'}"
             print(total_row)
         print("-" * WIDTH)
@@ -25,23 +24,25 @@ matrix_idx, step, empty_ceils = 0, 0, SIZE**SIZE
 print_field()
 
 while True:
-    if not empty_ceils:
-        break
-
     info = input("\n> ")
     if info.lower() == "end":
         break
 
-    x, y = map(int, info.split())
-    if not (0 <= x < SIZE and 0 <= y < SIZE):
-        print(f"Incorrect input coords ({x}; {y})")
-    elif field[matrix_idx][x][y] != " ":
-        print(f"Ceil ({x}; {y}) already matched")
-    else:
-        field[matrix_idx][x][y] = (CROSS, CIRCLE)[step]
-        step = (step + 1) % 2
-        matrix_idx = x * SIZE + y
-        empty_ceils -= 1
-
     os.system("cls" if os.name == "posix" else "clear")
+    try:
+        x, y = map(int, info.split())
+        if field[matrix_idx][x][y] != " ":
+            print(f"Ceil ({x}; {y}) already matched")
+        else:
+            field[matrix_idx][x][y] = (CROSS, CIRCLE)[step]
+            step = (step + 1) % 2
+            matrix_idx = x * SIZE + y
+            empty_ceils -= 1
+    except ValueError:    
+        print('Incorrect input')
+    except IndexError:
+        print(f"Incorrect input coords ({x}; {y})")
+
     print_field()
+    if not empty_ceils:
+        break
